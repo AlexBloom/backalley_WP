f0dacfc6105972576b562af7318cfdd7fa744acb
Alex Bloom <alexbloomdesign@gmail.com>
2017-07-31 08:59:47 -0700

    Add Local Install

---
+++ wp-content/themes/backalley/inc/custom-header.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Sample implementation of the Custom Header feature
+ *
+ * You can add an optional custom header image to header.php like so ...
+ *
+	<?php the_header_image_tag(); ?>
+ *
+ * @link https://developer.wordpress.org/themes/functionality/custom-headers/
+ *
+ * @package backalley
+ */
+
+/**
+ * Set up the WordPress core custom header feature.
+ *
+ * @uses backalley_header_style()
+ */
+function backalley_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'backalley_custom_header_args', array(
+		'default-image'          => '',
+		'default-text-color'     => '000000',
+		'width'                  => 1000,
+		'height'                 => 250,
+		'flex-height'            => true,
+		'wp-head-callback'       => 'backalley_header_style',
+	) ) );
+}
+add_action( 'after_setup_theme', 'backalley_custom_header_setup' );
+
+if ( ! function_exists( 'backalley_header_style' ) ) :
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see backalley_custom_header_setup().
+	 */
+	function backalley_header_style() {
+		$header_text_color = get_header_textcolor();
+
+		/*
+		 * If no custom options for text are set, let's bail.
+		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 */
+		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+			return;
+		}
+
+		// If we get this far, we have custom styles. Let's do this.
+		?>
+		<style type="text/css">
+		<?php
+		// Has the text been hidden?
+		if ( ! display_header_text() ) :
+		?>
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		<?php
+			// If the user has set a custom color for the text use that.
+			else :
+		?>
+			.site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?>
+		</style>
+		<?php
+	}
+endif;
